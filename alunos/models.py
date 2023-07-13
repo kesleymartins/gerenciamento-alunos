4f3685c4c868524d5b10cd92f1f95a4763cd9b0f
kesleymartins <kesley.martins@vivaldi.net>
2023-07-13 20:46:07 -0300

    create model Aluno

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    curso = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=20)
+    rg = models.CharField(max_length=20)
+
+    data_nascimento = models.DateField()
+    data_ingresso = models.DateField()
