00ef1ae840426a3a036ad346a27de0a1b7e5777d
kesleymartins <kesley.martins@vivaldi.net>
2023-07-14 02:51:44 -0300

    create base views

--- alunos/views.py
+++ alunos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import request
+from .forms import AlunoForm
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def criar_aluno(request):
+    form = AlunoForm()
+    context = { 'form': form }
+    return render(request, 'criar-aluno.html', context)
+
+def alunos_cadastrados(request):
+    context = {}
+    return render(request, 'alunos-cadastrados.html', context)
+
+def mostrar_aluno(request, pk):
+    context = {}
+    return render(request, 'mostrar-aluno.html', context)
