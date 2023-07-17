b33ec6979ebefbf46b057a72d35290ac159f0ffb
kesleymartins <kesley.martins@vivaldi.net>
2023-07-17 18:45:55 -0300

    update pk param name to id

--- alunos/urls.py
+++ alunos/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name="home_page"),
     path('criar-aluno', views.criar_aluno, name="criar_aluno"),
     path('alunos-cadastrados', views.alunos_cadastrados, name="alunos_cadastrados"),
-    path('mostrar-aluno/<str:pk>', views.mostrar_aluno, name="mostrar_aluno"),
+    path('mostrar-aluno/<str:id>', views.mostrar_aluno, name="mostrar_aluno"),
 ]
