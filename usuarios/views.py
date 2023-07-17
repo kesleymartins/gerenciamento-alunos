eb6302f7044c74127bab0ba49091a3f694b33a00
kesleymartins <kesley.martins@vivaldi.net>
2023-07-17 15:33:10 -0300

    create usuarios views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth import login as django_login
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+def cadastro(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return render(request, 'cadastro.html', {})
+
+        user = User.objects.create_user(username, "", password)
+        user.save()
+
+        django_login(request, user)
+
+        return redirect('alunos_cadastrados')
+
+    return render(request, 'cadastro.html', {})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            django_login(request, user)
+            return redirect('alunos_cadastrados')
+
+    return render(request, 'login.html', {})
+
+def logout(request):
+    django_logout(request)
+    return redirect('home_page')
