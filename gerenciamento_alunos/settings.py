8ef0e364a40d43763e74b738bc57caa3c5b06383
kesleymartins <kesley.martins@vivaldi.net>
2023-07-17 15:28:45 -0300

    install usuarios app

--- gerenciamento_alunos/settings.py
+++ gerenciamento_alunos/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "alunos.apps.AlunosConfig",
+    "usuarios.apps.UsuariosConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
